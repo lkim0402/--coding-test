690ae91b1fa3e2380dc37bd4c566c41f3de01ef8
JihyeonLee <devlee1011@gmail.com>
2026-01-27 18:07:39 +0900

    feat(week1): Question3

--- src/jihyeon/Main.java
+++ src/jihyeon/Main.java
@@ -1,16 +1,16 @@
 package jihyeon;
 
 import jihyeon.util.Generator;
-import jihyeon.week1.Question2;
+import jihyeon.week1.Question3;
 
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = Generator.generateRandomArray();
-        Question2 q2 = new Question2();
-        int[] solution = q2.solution(array);
+        int[] array = Generator.generateRandomSmallArray();
+        Question3 q3 = new Question3();
+        int[] solution = q3.solution(array);
         System.out.println(Arrays.toString(solution));
     }
 }
--- src/jihyeon/util/Generator.java
+++ src/jihyeon/util/Generator.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class Generator {
 
-    public static int[] generateRandomArray() {
+    // 2 ~ 100000
+    public static int[] generateRandomBigArray() {
         Random random = new Random();
 
         // 배열 크기: 2 ~ 100000
@@ -15,6 +16,22 @@ public class Generator {
         // 값 범위: -100000 ~ 100000
         for (int i = 0; i < size; i++) {
             arr[i] = random.nextInt(200001) - 100000;
+        }
+        return arr;
+    }
+
+    // 2 ~ 100
+    public static int[] generateRandomSmallArray() {
+        Random random = new Random();
+
+        // 배열 크기: 2 ~ 100
+        int size = random.nextInt(100 - 2 + 1) + 2;
+
+        int[] arr = new int[size];
+
+        // 값 범위: 0 ~ 100
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt(101); // 0 ~ 100
         }
         return arr;
     }
