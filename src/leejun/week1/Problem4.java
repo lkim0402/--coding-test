dffcb7654a3a359691e0663a8c54abdea3fdf5ff
Leejun <estherleejunkim@gmail.com>
2026-01-27 22:46:44 +0900

    문제 4 풀이

---
+++ src/leejun/week1/Problem4.java
@@ -0,0 +1,52 @@
+package leejun.week1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem4 {
+
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(solution(new int[]{1,2,3,4,5})));
+    System.out.println(Arrays.toString(solution(new int[]{1,3,2,4,2})));
+  }
+
+  public static int[] solution(int[] answers) {
+    int[][] patterns = {
+        {1,2,3,4,5},
+        {2,1,2,3,2,4,2,5},
+        {3,3,1,1,2,2,4,4,5,5}
+    };
+        /*
+        답안    {1,2,3,4,5,6}
+        수포자1 {1,2,3,4,5}
+
+        */
+    int[] count = new int[3];
+
+    for (int i = 0; i < 3; i++) {
+      int[] pattern = patterns[i];
+      int cur = 0;
+      int cnt = 0;
+      while (cur < answers.length) {
+        if (pattern[cur % pattern.length] == answers[cur]) {
+          cnt++;
+        }
+        cur++;
+      }
+      count[i] = cnt;
+    }
+
+    int maxCount = Arrays.stream(count).max().getAsInt();
+    List<Integer> res = new ArrayList<>();
+    for (int i = 0; i < count.length; i++) {
+      if (count[i] == maxCount) {
+        res.add(i+1);
+      }
+    }
+    return res.stream()
+        .mapToInt(Integer::intValue)
+        .toArray();
+  }
+
+}
